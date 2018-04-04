0d4e0c3254d9755cf93156a7b797bf180e974ee5
Spandan Chatterjee <chatterjee@apple.com>
2018-04-04 14:58:08 -0700

    Style Changes

--- js/app.js
+++ js/app.js
@@ -1,10 +1,29 @@
 $(function(){
-    var form = document.querySelector('#numControlPickers');
+    const form = document.querySelector('#numControlPickers');
  
     function drawGrid(evt){
-        console.log("Make Grid");
+        console.log("Draw Grid");
+
+        const rows = parseInt(document.getElementById("hghtIp").value);
+        const cols = parseInt(document.getElementById("wdthIp").value);
+        debugger;
+        for(let iCount = 1; iCount <= rows; iCount++){
+            $("#dynTableCanvas").append("<tr></tr>");
+            for(let jCount = 1; jCount <= cols; jCount++){
+                $("tr:last").append("<td></td>");
+                $("td").attr("class", "pixel");
+            }
+        }
     }
 
+    $('#dynTableCanvas').click('td', function paint (evt) {
+        $(evt.target).css('background-color', $('#colorIp').val());
+    });
+
+    $("#btnReset").click(function(){
+        $('#dynTableCanvas').find("tr").remove();
+        $('#dynTableCanvas').removeAttr("style")
+    });
 
     form.addEventListener('submit', function (e) {
         e.preventDefault();
